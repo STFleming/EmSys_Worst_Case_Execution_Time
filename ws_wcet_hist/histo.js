6c0d5d69d65e7ef7f8947da5b971c4c05056de9d
Shane T. Fleming <t-shflem@microsoft.com>
2020-12-30 15:26:27 +0000

    Adding the percentile latency breakdowns -- still somewhat broken

--- ws_wcet_hist/histo.js
+++ ws_wcet_hist/histo.js
@@ -29,13 +29,80 @@ class histo{
                         this.bins[i] = 0;
                 }
 
+                // for tracking the cumulative total in the bins
+                this.total_samples = 0;
+                this.cumulative_bins = [ ]
+
                 this.clearDrawing(); // clear any currently drawn bins
 
                 this.draw();
         }
 
+        // Creates the cumulative totals bins
+        cumulativeTotalBinCalc() {
+                this.total_sample = 0;
+                for(var i=0; i<this.numBins; i++) {
+                        this.total_samples += this.bins[i];
+                        this.cumulative_bins[i] = this.total_samples;
+                }
+        }
+
+        // Get percentile bin -- returns the bin index for a given percentile with the
+        // current data
+        getPercentileBin(percentile) {
+                var target_val = percentile*this.total_samples;
+                console.log("Target Val = " + target_val);
+                for(var i=0; i<this.numBins; i++) {
+                        if(this.cumulative_bins[i] > target_val)
+                                return i-1;
+                }
+        }
+
+        // Gets the value at a bin index
+        binVal(idx) {
+                return this.minBin + idx*this.binSize;
+        }
+
+        // gets the x pos of a bin index
+        binPos(idx) {
+                return this.x + idx*(this.binWidth + this.binSpace);
+        }
+
+        // Draws a dashed line at the percentile point
+        drawPercentile(percentile) {
+                var idx = this.getPercentileBin(percentile);
+                var label = (percentile*100) + "% = " + this.binVal(idx) + " ms";
+                if(!Number.isNaN(this.binVal(idx)))
+                        this.drawInfo(label, this.binPos(idx));
+        }
+
+        // Add an info line
+        drawInfo(text, x) {
+                // Draw the vertical line                
+                this.svg.append("line")
+                        .attr("id", this.name+"_histbin")
+                        .attr("x1", x)
+                        .attr("y1", this.y + this.maxHeight + 15)
+                        .attr("x2", x)
+                        .attr("y2", this.y)
+                        .attr("stroke-width", 1)
+                        .attr("stroke", this.colour.highlight)
+                        .attr("stroke-dasharray", ("3,3"));
+
+                this.svg.append("text")
+                        .attr("id", this.name+"_histbin")
+                        .attr("font-family", "sans-serif")
+                        .attr("font-size", "12px")
+                        .attr("fill", this.colour.highlight)
+                        .attr("x", x - 10)
+                        .attr("y", this.y + this.maxHeight + 30)
+                        .text(text);
+        }
+
+        // Draws the histogram from the current data stored
         draw() {
                 this.clearDrawing();
+                this.cumulativeTotalBinCalc();
 
                 // scale
                 var maxv = this.maxVal();
@@ -50,6 +117,14 @@ class histo{
                         .attr("width", this.binWidth)
                         .attr("height", hist_linear_scale(this.bins[i]));
                 }
+
+                // info lines on the histogram
+                this.drawInfo(this.minBin + " ms", this.x);
+                this.drawInfo(" >= "+this.maxBin + " ms", this.x + this.numBins*(this.binWidth + this.binSpace));
+
+                // Percentiles
+                this.drawPercentile(0.5);
+
         }
 
         // deletes all the histogram bins shown rendered on the display
